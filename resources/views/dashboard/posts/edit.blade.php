ccd220f6fc35ec026ce8429d067689d2351707f3
Abraham Megantoro Samudra <104062877+abrahammegantoro@users.noreply.github.com>
2022-08-06 12:16:55 +0700

    Update edit.blade.php

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -28,7 +28,7 @@
           @enderror
         </div>
         <div class="mb-3">
-          <label for="category" class="form-label">Categories</label>
+          <label for="category" class="form-label">Category</label>
           <select class="form-select" name="category_id">
             @foreach ($categories as $category)
               @if(old('category_id', $post->category_id) == $category->id) <!-- == aja biar ga ngecek tipe datanya -->
